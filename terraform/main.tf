6d6574633185d330f73a3b4f89fea236164e0a7b
Isabel Lima <bellima93@gmail.com>
2023-03-26 18:06:00 -0300

    feat: add internet access to vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,6 +21,19 @@ resource "aws_vpc" "ada_vpc" {
   }
 }
 
+resource "aws_internet_gateway" "ada_igw" {
+  vpc_id = aws_vpc.ada_vpc.id
+}
+
+resource "aws_route_table" "ada_route_table" {
+  vpc_id = aws_vpc.ada_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ada_igw.id
+  }
+}
+
 resource "aws_subnet" "ada_public_1a" {
     vpc_id = aws_vpc.ada_vpc.id
     cidr_block = "10.0.0.0/27"
@@ -79,6 +92,21 @@ resource "aws_subnet" "ada_private_1c" {
     tags = {
         Name = "ada_private_1c"
     }
+}
+
+resource "aws_route_table_association" "ada_public_1a_subnet_association" {
+  subnet_id      = aws_subnet.ada_public_1a.id
+  route_table_id = aws_route_table.ada_route_table.id
+}
+
+resource "aws_route_table_association" "ada_public_1b_subnet_association" {
+  subnet_id      = aws_subnet.ada_public_1b.id
+  route_table_id = aws_route_table.ada_route_table.id
+}
+
+resource "aws_route_table_association" "ada_public_1c_subnet_association" {
+  subnet_id      = aws_subnet.ada_public_1c.id
+  route_table_id = aws_route_table.ada_route_table.id
 }
 
 resource "aws_security_group" "backend_sg" {
